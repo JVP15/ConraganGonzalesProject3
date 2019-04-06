019481b548ee0a3cc6dd9eaacefea7c6c0e6ca10
DJGonzales96 <devin.j.gonzales@gmail.com>
2019-04-06 11:52:50 -0700

    added documentation for Maze and Cell

--- src/cs146S19/conragangonzales/project3/Cell.java
+++ src/cs146S19/conragangonzales/project3/Cell.java
@@ -3,12 +3,11 @@ package cs146S19.conragangonzales.project3;
 import java.util.ArrayList;
 
 /**
- * Models a 1x1 cell for use in a grid. Each cell object keeps track of
- * the location and state of nieghboring cells.
+ * Models a 1x1 cell for use in a grid. Each cell object keeps track of the
+ * location and state of nieghboring cells.
  */
 
-public class Cell
-{	
+public class Cell {
 	// Neighboring cell positions
 	private Cell north;
 	private Cell east;
@@ -18,12 +17,11 @@ public class Cell
 	private ArrayList<Cell> doorways;
 	// True if every cell wall is intact, otherwise false
 	private boolean wallsIntact;
-	
+
 	/**
 	 * Cell ctor initializes cell and properties.
 	 */
-	public Cell()
-	{
+	public Cell() {
 		north = null;
 		south = null;
 		east = null;
@@ -31,74 +29,65 @@ public class Cell
 		wallsIntact = true;
 		doorways = new ArrayList<>();
 	}
-	
-	public Cell getNorth()
-	{
+
+	public Cell getNorth() {
 		return north;
 	}
-	
-	public Cell getSouth()
-	{
+
+	public Cell getSouth() {
 		return south;
 	}
-	
-	public Cell getEast()
-	{
+
+	public Cell getEast() {
 		return east;
 	}
-	
-	public Cell getWest()
-	{
+
+	public Cell getWest() {
 		return west;
 	}
-	
-	
-	public void setNorth(Cell other)
-	{
+
+	public void setNorth(Cell other) {
 		north = other;
 	}
-	
-	public void setSouth(Cell other)
-	{
+
+	public void setSouth(Cell other) {
 		south = other;
 	}
-	
-	public void setEast(Cell other)
-	{
+
+	public void setEast(Cell other) {
 		east = other;
 	}
-	
-	public void setWest(Cell other)
-	{
+
+	public void setWest(Cell other) {
 		west = other;
 	}
-	
-	public boolean areWallsIntact()
-	{
+
+	public boolean areWallsIntact() {
 		return wallsIntact;
 	}
+
 	/**
 	 * Deletes the wall of a specified neighboring cell and adds it to doorways.
-	 * @param 	cell	neighboring cell to connect to current cell
+	 * 
+	 * @param cell neighboring cell to connect to current cell
 	 */
-	public void knockDownWall(Cell cell)
-	{
+	public void knockDownWall(Cell cell) {
 		doorways.add(cell);
 		wallsIntact = false;
 	}
+
 	/**
 	 * Checks if a cell is connected to a neighboring cell.
-	 * @param 	cell	neighboring cell
-	 * @return	true 	if both cell objects share a pathway
-	 * 			false 	if neither cell shares a path
+	 * 
+	 * @param cell neighboring cell
+	 * @return true if both cell objects share a pathway false if neither cell
+	 *         shares a path
 	 */
-	public boolean hasDoorwayTo(Cell cell)
-	{
+	public boolean hasDoorwayTo(Cell cell) {
 		boolean doorway = false;
-		
-		for(Cell c : doorways)
-		{
-			if(c == cell)
+
+		for (Cell c : doorways) {
+			if (c == cell)
 				doorway = true;
 		}
 		return doorway;
--- src/cs146S19/conragangonzales/project3/Maze.java
+++ src/cs146S19/conragangonzales/project3/Maze.java
@@ -5,145 +5,133 @@ import java.util.Random;
 import java.util.Stack;
 
 /**
- * Models and prints a maze as a specified nxn grid of cells. Solutions are 
+ * Models and prints a maze as a specified nxn grid of cells. Solutions are
  * randomly generated pathways with exactly one entrance and exit.
  */
-public class Maze
-{
+public class Maze {
 	private Cell[][] cells;
 	private int width;
 	private int height;
-	
+
 	/**
 	 * Maze ctor creates a maze with specified width and height.
-	 * @param w  width of the maze
-	 * @param h  height of the maze
+	 * 
+	 * @param w width of the maze
+	 * @param h height of the maze
 	 */
-	public Maze(int w, int h)
-	{
+	public Maze(int w, int h) {
 		width = w;
 		height = h;
 		cells = new Cell[width][height];
 		// Create grid of cells
 		initializeCells();
 		// Construct the maze
-		createMaze();		
+		createMaze();
 	}
-	
+
 	/**
-	 * Prints an ASCII formatted representation of a maze.
-	 * '+' represents cell intersections and corners
-	 * '|' and '-' represents cell walls 
+	 * Prints an ASCII formatted representation of a maze. '+' represents cell
+	 * intersections and corners '|' and '-' represents cell walls
 	 */
-	public void printMaze()
-	{
+	public void printMaze() {
 		System.out.print("+  +");
-		for(int i = 1; i < width; i++)
-		{
+		for (int i = 1; i < width; i++) {
 			System.out.print("--+");
 		}
 		System.out.println();
-		
-		for(int i = 0; i < height; i++)
-		{
-			for(Cell c : cells[i])
-			{
+
+		for (int i = 0; i < height; i++) {
+			for (Cell c : cells[i]) {
 				// if cells are connected, no wall is printed inbetween them
-				if(c.hasDoorwayTo(c.getWest()))
-					System.out.print(" " );
+				if (c.hasDoorwayTo(c.getWest()))
+					System.out.print(" ");
 				else
 					System.out.print("|");
-				
+
 				System.out.print("  ");
 			}
-			
+
 			System.out.println("|");
-			
-			for(Cell c : cells[i])
-			{
+
+			for (Cell c : cells[i]) {
 				System.out.print("+");
-				
-				if(c.hasDoorwayTo(c.getSouth()))
-					System.out.print("  " );
+
+				if (c.hasDoorwayTo(c.getSouth()))
+					System.out.print("  ");
 				else
 					System.out.print("--");
 			}
 			System.out.println("+");
 		}
 	}
+
 	/**
 	 * Constructs the grid of cells specified in the Maze constructor.
 	 */
-	private void initializeCells()
-	{
+	private void initializeCells() {
 		// Construct the grid
-		for(int i = 0; i < height; i++)
-			for(int j = 0; j < width; j++)
+		for (int i = 0; i < height; i++)
+			for (int j = 0; j < width; j++)
 				cells[i][j] = new Cell();
 		// Set the correct neighbors of each cell in the grid
-		for(int i = 0; i < height; i++)
-		{
-			for(int j = 0; j < width; j++)
-			{
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
 				Cell current = cells[i][j];
-				if(i - 1 >= 0)
-					current.setNorth(cells[i-1][j]);
-				if(i + 1 < height)
-					current.setSouth(cells[i+1][j]);
-				if(j - 1 >= 0)
-					current.setWest(cells[i][j-1]);
-				if(j + 1 < width)
-					current.setEast(cells[i][j+1]);
+				if (i - 1 >= 0)
+					current.setNorth(cells[i - 1][j]);
+				if (i + 1 < height)
+					current.setSouth(cells[i + 1][j]);
+				if (j - 1 >= 0)
+					current.setWest(cells[i][j - 1]);
+				if (j + 1 < width)
+					current.setEast(cells[i][j + 1]);
 			}
 		}
 	}
+
 	/**
 	 * The Maze generation algorithm constructs the maze using DFS on the cell grid.
 	 */
-	private void createMaze()
-	{
+	private void createMaze() {
 		Random rand = new Random();
-		
+
 		Stack<Cell> cellStack = new Stack<>();
 		int totalCells = cells.length * cells[0].length;
 		int visitedCells = 1;
 		Cell current = cells[0][0];
-		
-		while(visitedCells < totalCells)
-		{
+
+		while (visitedCells < totalCells) {
 			ArrayList<Cell> adjacentCells = new ArrayList<>();
 			// Find all nieghbors of the current cell with intact walls
-			if(current.getNorth() != null && current.getNorth().areWallsIntact() == true)
-				adjacentCells.add( current.getNorth());
-			
-			if(current.getSouth() != null && current.getSouth().areWallsIntact() == true)
-				adjacentCells.add( current.getSouth());
-			
-			if(current.getEast() != null && current.getEast().areWallsIntact() == true)
-				adjacentCells.add( current.getEast());
-			
-			if(current.getWest() != null && current.getWest().areWallsIntact() == true)
-				adjacentCells.add( current.getWest());
-			
+			if (current.getNorth() != null && current.getNorth().areWallsIntact() == true)
+				adjacentCells.add(current.getNorth());
+
+			if (current.getSouth() != null && current.getSouth().areWallsIntact() == true)
+				adjacentCells.add(current.getSouth());
+
+			if (current.getEast() != null && current.getEast().areWallsIntact() == true)
+				adjacentCells.add(current.getEast());
+
+			if (current.getWest() != null && current.getWest().areWallsIntact() == true)
+				adjacentCells.add(current.getWest());
+
 			// If an intact cell nieghbor is found
-			if(adjacentCells.size() > 0)
-			{
-				// Choose a neighboring cell at random, knock down its wall, and repeat for the modified cell
-				Cell randomAdjacentCell = adjacentCells.get( rand.nextInt( adjacentCells.size() ) );
+			if (adjacentCells.size() > 0) {
+				// Choose a neighboring cell at random, knock down its wall, and repeat for the
+				// modified cell
+				Cell randomAdjacentCell = adjacentCells.get(rand.nextInt(adjacentCells.size()));
 				cellStack.push(current);
 				current.knockDownWall(randomAdjacentCell);
 				randomAdjacentCell.knockDownWall(current);
-				current = randomAdjacentCell;			
+				current = randomAdjacentCell;
 				visitedCells++;
-			}
-			else
-			{
+			} else {
 				current = cellStack.pop();
 			}
 		}
-		// Create the maze exit 
+		// Create the maze exit
 		Cell end = new Cell();
-		cells[width-1][height-1].setSouth(end);
-		cells[width-1][height-1].knockDownWall(end);
+		cells[width - 1][height - 1].setSouth(end);
+		cells[width - 1][height - 1].knockDownWall(end);
 	}
 }
