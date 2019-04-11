2b01ef8086f850135cfd0f66c5c407a04e97cfb0
JVP15 <jordanvonpordan@gmail.com>
2019-04-11 16:40:09 -0700

    Changed the way Cell stores and accesses doorways.
    Currently breaks everything, but that will be fixed soon.

--- src/cs146S19/conragangonzales/project3/Cell.java
+++ src/cs146S19/conragangonzales/project3/Cell.java
@@ -7,93 +7,59 @@ import java.util.ArrayList;
  * location and state of nieghboring cells.
  */
 
-public class Cell {
-	// Neighboring cell positions
-	private Cell north;
-	private Cell east;
-	private Cell south;
-	private Cell west;
+public class Cell
+{
+	public enum Direction { NORTH, SOUTH, EAST, WEST };
+
 	// List of any neighboring cells without intact walls
-	private ArrayList<Cell> doorways;
+	private Cell[] doorways;
 	// True if every cell wall is intact, otherwise false
 	private boolean wallsIntact;
 
 	/**
 	 * Cell ctor initializes cell and properties.
 	 */
-	public Cell() {
-		north = null;
-		south = null;
-		east = null;
-		west = null;
-		wallsIntact = true;
-		doorways = new ArrayList<>();
+	public Cell()
+	{
+		doorways = new Cell[] {null, null, null, null};
+
+		wallsIntact = true;		
 	}
 
-	public Cell getNorth() {
-		return north;
-	}
-
-	public Cell getSouth() {
-		return south;
-	}
-
-	public Cell getEast() {
-		return east;
-	}
-
-	public Cell getWest() {
-		return west;
-	}
-
-	public void setNorth(Cell other) {
-		north = other;
-	}
-
-	public void setSouth(Cell other) {
-		south = other;
-	}
-
-	public void setEast(Cell other) {
-		east = other;
-	}
-
-	public void setWest(Cell other) {
-		west = other;
-	}
-
-	public boolean areWallsIntact() {
+	public boolean areWallsIntact()
+	{
 		return wallsIntact;
 	}
 
-	/**
-	 * Deletes the wall of a specified neighboring cell and adds it to doorways.
-	 * 
-	 * @param cell neighboring cell to connect to current cell
-	 */
-	public void knockDownWall(Cell cell) {
-		doorways.add(cell);
-		wallsIntact = false;
-	}
 
-	/**
-	 * Checks if a cell is connected to a neighboring cell.
-	 * 
-	 * @param cell neighboring cell
-	 * @return true if both cell objects share a pathway false if neither cell
-	 *         shares a path
-	 */
-	public boolean hasDoorwayTo(Cell cell) {
-		boolean doorway = false;
-
-		for (Cell c : doorways) {
-			if (c == cell)
-				doorway = true;
+	public void openDoorwayToo(Cell cell, Direction d)
+	{
+		switch (d)
+		{
+			case NORTH: 
+				doorways[0] = cell; wallsIntact = false; break;
+			case EAST:
+				doorways[1] = cell; wallsIntact = false; break;
+			case WEST: 
+				doorways[2] = cell; wallsIntact = false; break;
+			case SOUTH: 
+				doorways[3] = cell; wallsIntact = false; break;
 		}
-		return doorway;
 	}
-	
-	public ArrayList<Cell> getDoorways() {
-		return doorways;
+
+
+	public boolean hasDoorwayTo(Direction d)
+	{
+		if(d == Direction.NORTH && doorways[0] != null)
+			return true; 
+		else if(d == Direction.EAST && doorways[1] != null)
+			return true; 
+		else if(d == Direction.WEST && doorways[2] != null)
+			return true; 
+		else if(d == Direction.SOUTH && doorways[3] != null)
+			return true; 
+		
+		else
+			return false;
 	}
 }
